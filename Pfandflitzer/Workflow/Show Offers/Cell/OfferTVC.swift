d26323b5bdda4fe2281774650c47ceaf8e0633e7
Benjamin Herzog <mail@benchr.de>
2016-11-12 12:40:17 +0100

    synx

---
+++ Pfandflitzer/Workflow/Show Offers/Cell/OfferTVC.swift
@@ -0,0 +1,15 @@
+//
+//  OfferTVC.swift
+//  Pfandflitzer
+//
+//  Created by Benjamin Herzog on 12/11/2016.
+//  Copyright © 2016 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class OfferTVC: UITableViewCell {
+
+
+    
+}
