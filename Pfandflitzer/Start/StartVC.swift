eb1efbe1e708929805bd34a1f775cc9714b4efde
Benjamin Herzog <mail@benchr.de>
2016-11-12 09:49:19 +0100

    Added loca, synx project

---
+++ Pfandflitzer/Start/StartVC.swift
@@ -0,0 +1,17 @@
+//
+//  StartVC.swift
+//  Pfandflitzer
+//
+//  Created by Benjamin Herzog on 12/11/2016.
+//  Copyright © 2016 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class StartVC: ViewController {
+
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var offerButton: UIButton!
+    
+    
+}
