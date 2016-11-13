aed46f57db0aeb7f112159b5a07a73097d363e81
Benjamin Herzog <mail@benchr.de>
2016-11-13 12:53:33 +0100

    Added alert

--- Pfandflitzer/Extensions/Alert.swift
+++ Pfandflitzer/Extensions/Alert.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2016 Benjamin Herzog. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Alert {
+    
+    let message: String
+    var actions = [UIAlertAction]()
+    
+    init(message: String) {
+        self.message = message
+    }
+    
+    func action(title: String, handler: @escaping () -> Void) -> Alert {
+        actions.append(UIAlertAction(title: title, style: .default, handler: { _ in
+            handler()
+        }))
+        return self
+    }
+    
+    func show(on controller: UIViewController) {
+        let a = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        actions.forEach(a.addAction)
+        controller.present(a, animated: true, completion: nil)
+    }
+    
+}
